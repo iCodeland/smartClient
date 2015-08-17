dccebdea887b72255e8978a3bda80037c3b2cb15
ZeroBo <103021036@live.asia.edu.tw>
2015-08-17 15:51:54 +0800

    JScrollPane

--- src/Frame1.java
+++ src/Frame1.java
@@ -2,58 +2,62 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class Connection extends JFrame{
+public class Frame1 extends JFrame {
 
 	Container contentPane;
+	int Width = 300;
+	int Height = 350;
 	
-	int Width = 230;
-	int Height = 170;
 	
-	Connection () {
+	Frame1 () {
 		
+		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		this.setBounds((int)((screenSize.getWidth()-Width)*0.5), (int)((screenSize.getHeight()-Height)*0.5), Width, Height);
+				
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
 		contentPane = this.getContentPane();
 		
-
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		contentPane = this.getContentPane();
-		
-		Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-		this.setBounds((int)((screenSize.getWidth()-Width)*0.5), (int)((screenSize.getHeight()-Height)*0.5), Width, Height);
-		
+//		setBounds(400, 400, 300, 300);
 		setLayout(null);
 		
 		JLabel jplIP = new JLabel("IP  :");
-		jplIP.setBounds(25, 15, 100, 23);
+		jplIP.setBounds(65, 15, 100, 23);
 		jplIP.setFont(new Font("標楷體", Font.PLAIN, 20));
 		contentPane.add(jplIP);
 		
 		JLabel jplPORT = new JLabel("PORT :");
-		jplPORT.setBounds(15, 55, 100, 23);
+		jplPORT.setBounds(55, 55, 100, 23);
 		jplPORT.setFont(new Font("標楷體", Font.PLAIN, 20));
 		contentPane.add(jplPORT);
-		
+				
 		JTextField jtfIP = new JTextField();
-		jtfIP.setBounds(80, 15, 120, 25);
+		jtfIP.setBounds(120, 15, 120, 25);
 		jtfIP.setFont(new Font("標楷體", Font.PLAIN, 20));
 		contentPane.add(jtfIP);
 		
 		JTextField jtfPORT = new JTextField();
-		jtfPORT.setBounds(80, 55, 120, 25);
+		jtfPORT.setBounds(120, 55, 120, 25);
 		jtfPORT.setFont(new Font("標楷體", Font.PLAIN, 20));
 		contentPane.add(jtfPORT);
 		
-		JButton jbtcon = new JButton("連線");
-		jbtcon.setBounds(50, 96, 120, 23);
+		JButton jbtcon = new JButton("Connection");
+		jbtcon.setBounds(70, 260, 150, 23);
 		jbtcon.setFont(new Font("標楷體", Font.PLAIN, 20));
 		contentPane.add(jbtcon);
 		
+		JTextArea jta = new JTextArea();
+//		jta.setBounds(25, 90, 240, 150);
+//		contentPane.add(jta);
 		
-		setTitle("Client");
+		JScrollPane jsp = new JScrollPane(jta);
+		jsp.setBounds(25, 90, 240, 150);
+		contentPane.add(jsp);
+		
 		setVisible(true);
 		
 	}
 	
+	
+
 }
--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ public class main {
 
 	public static void main(String[] args) {
 				
-		Connection con = new Connection();
-		
+		//Connection con = new Connection();
+		Frame1 ff = new Frame1();
 	}
 
 }
